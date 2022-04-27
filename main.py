9b41d171c863f7255924c1d21d828f155a8de27d
Liam Attard <liamattard@pop-os.localdomain>
2022-04-27 18:47:50 +0200

    Started processing like S.O.T.A

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import Data_Processing.tensorflow_pipeline as tp
-import RetrievalSystem.retrieval as retrieval
+import Data_Processing.SOTA_pipeline as sotap
 
 
 def main():
     # Currently only using 1000000 values
 
-    user_medicine_dataset, medicine_dataset = tp.start(should_create=False)
+    # user_medicine_dataset, medicine_dataset = tp.start(should_create=False)
     # retrieval.start_train(user_medicine_dataset, medicine_dataset)
-    retrieval.import_model('Data/model')
+    sotap.start(from_sota=True)
+    # retrieval.import_model('Data/model')
 
 
 if __name__ == "__main__":
